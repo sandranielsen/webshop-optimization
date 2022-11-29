8edef79caa0254d0a7f2fc251451c576a8f8442c
sandranielsen <eaasmmn@students.eaaa.dk>
2022-11-29 15:37:58 +0100

    netlify plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,15 @@ import "./src/styles/global.css";
 import CombinedProvider from "./src/context/CombinedProvider";
 
 export const wrapRootElement = CombinedProvider;
+
+// Checking for sw updates
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
